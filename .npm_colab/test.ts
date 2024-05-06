1e4167cf51fd792ed15f28876f2b258df8d36c09
nagendra <nagendra10082003@gmail.com>
2024-05-06 21:53:16 +0530

    test: checking

--- .npm_colab/test.ts
+++ .npm_colab/test.ts
@@ -1,13 +1,24 @@
-enum EEnum1 {
-  hello,
+enum EEnum1{
+    hello
 }
-enum EEnum2 {
-  Abc,
+enum EEnum2{
+    Abc
 }
 
-type TLove = "hell";
-interface ILearnHunger {}
-class LearnHunger {}
-abstract class ALearnHunger {}
-export namespace NLearnHunger {}
-const C_LEARN_HUNGER = "LEARN_HUNGER";
+type TLove="hell"
+interface ILearnHunger{
+
+}
+class LearnHunger{}
+abstract class ALearnHunger{
+
+}
+export namespace NLearnHunger{}
+const C_LEARN_HUNGER="LEARN_HUNGER";
+
+
+
+
+
+
+
